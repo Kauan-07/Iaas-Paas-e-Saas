1daad51ebc66d63d2b1e34bf37a0092e087f8581
Kauan-07 <160788373+Kauan-07@users.noreply.github.com>
2024-08-22 18:31:07 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.service');
 
+    sections.forEach(section => {
+        section.addEventListener('click', () => {
+            alert(`Você clicou em ${section.querySelector('h2').innerText}`);
+        });
+    });
+});
